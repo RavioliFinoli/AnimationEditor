1f85c5964e7694698d96aa69564d28c1b01c89aa
Elias Finoli <elle_finoli@hotmail.com>
2018-10-13 13:42:39 +0200

    Added some stuff in application class

--- AnimationEditor/AnimationEditorApplication.cpp
+++ AnimationEditor/AnimationEditorApplication.cpp
@@ -1,6 +1,7 @@
 #include "AnimationEditorApplication.h"
 
-
+ComPtr<ID3D11Device> AnimationEditorApplication::gDevice = nullptr;
+ComPtr<ID3D11DeviceContext> AnimationEditorApplication::gDeviceContext = nullptr;
 
 AnimationEditorApplication::AnimationEditorApplication()
 {
--- AnimationEditor/AnimationEditorApplication.h
+++ AnimationEditor/AnimationEditorApplication.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <wrl.h>
+using Microsoft::WRL::ComPtr;
+
 class AnimationEditorApplication
 {
 public:
 	AnimationEditorApplication();
 	~AnimationEditorApplication();
+
+	static ComPtr<ID3D11Device> gDevice;
+	static ComPtr<ID3D11DeviceContext> gDeviceContext;
+private:
+
 };
 
