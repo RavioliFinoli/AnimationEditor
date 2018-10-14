22f09a78181a9ee53862a1bd9b9fe3b6b88efaac
Elias Finoli <elle_finoli@hotmail.com>
2018-10-14 17:52:57 +0200

    main using mouse input for camera orientation

--- AnimationEditor/main.cpp
+++ AnimationEditor/main.cpp
@@ -7,6 +7,12 @@
 #define UNICODE
 #endif 
 
+#ifndef HID_USAGE_PAGE_GENERIC
+#define HID_USAGE_PAGE_GENERIC         ((USHORT) 0x01)
+#endif
+#ifndef HID_USAGE_GENERIC_MOUSE
+#define HID_USAGE_GENERIC_MOUSE        ((USHORT) 0x02)
+#endif
 
 #include <windows.h>
 #include <d3dcompiler.h>
@@ -17,207 +23,7 @@
 HWND InitWindow(HINSTANCE hInstance);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// Most directX Objects are COM Interfaces
-// https://es.wikipedia.org/wiki/Component_Object_Model
-IDXGISwapChain* gSwapChain = nullptr;
-
-// Device and DeviceContext are the most common objects to
-// instruct the API what to do. It is handy to have a reference
-// to them available for simple applications.
-ID3D11Device* gDevice = nullptr;
-ID3D11DeviceContext* gDeviceContext = nullptr;
-
-// A "view" of a particular resource (the color buffer)
-ID3D11RenderTargetView* gBackbufferRTV = nullptr;
-
-// a resource to store Vertices in the GPU
-ID3D11Buffer* gVertexBuffer = nullptr;
-
-ID3D11InputLayout* gVertexLayout = nullptr;
-
-// resources that represent shaders
-ID3D11VertexShader* gVertexShader = nullptr;
-ID3D11PixelShader* gPixelShader = nullptr;
-
-HRESULT CreateShaders()
-{
-	// Binary Large OBject (BLOB), for compiled shader, and errors.
-	ID3DBlob* pVS = nullptr;
-	ID3DBlob* errorBlob = nullptr;
-
-	// https://msdn.microsoft.com/en-us/library/windows/desktop/hh968107(v=vs.85).aspx
-	HRESULT result = D3DCompileFromFile(
-		L"BasicVS.hlsl", // filename
-		nullptr,		// optional macros
-		nullptr,		// optional include files
-		"VS_main",		// entry point
-		"vs_5_0",		// shader model (target)
-		D3DCOMPILE_DEBUG,	// shader compile options (DEBUGGING)
-		0,				// IGNORE...DEPRECATED.
-		&pVS,			// double pointer to ID3DBlob		
-		&errorBlob		// pointer for Error Blob messages.
-	);
-
-	// compilation failed?
-	if (FAILED(result))
-	{
-		if (errorBlob)
-		{
-			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
-			// release "reference" to errorBlob interface object
-			errorBlob->Release();
-		}
-		if (pVS)
-			pVS->Release();
-		return result;
-	}
-
-	gDevice->CreateVertexShader(
-		pVS->GetBufferPointer(),
-		pVS->GetBufferSize(),
-		nullptr,
-		&gVertexShader
-	);
-
-	// create input layout (verified using vertex shader)
-	// Press F1 in Visual Studio with the cursor over the datatype to jump
-	// to the documentation online!
-	// please read:
-	// https://msdn.microsoft.com/en-us/library/windows/desktop/bb205117(v=vs.85).aspx
-	D3D11_INPUT_ELEMENT_DESC inputDesc[] = {
-		{
-			"POSITION",		// "semantic" name in shader
-			0,				// "semantic" index (not used)
-			DXGI_FORMAT_R32G32B32_FLOAT, // size of ONE element (3 floats)
-			0,							 // input slot
-			0,							 // offset of first element
-			D3D11_INPUT_PER_VERTEX_DATA, // specify data PER vertex
-			0							 // used for INSTANCING (ignore)
-		},
-		{
-			"COLOR",
-			0,				// same slot as previous (same vertexBuffer)
-			DXGI_FORMAT_R32G32B32_FLOAT,
-			0,
-			12,							// offset of FIRST element (after POSITION)
-			D3D11_INPUT_PER_VERTEX_DATA,
-			0
-		},
-	};
-
-	gDevice->CreateInputLayout(inputDesc, ARRAYSIZE(inputDesc), pVS->GetBufferPointer(), pVS->GetBufferSize(), &gVertexLayout);
-
-	// we do not need anymore this COM object, so we release it.
-	pVS->Release();
-
-	//create pixel shader
-	ID3DBlob* pPS = nullptr;
-	if (errorBlob) errorBlob->Release();
-	errorBlob = nullptr;
-
-	result = D3DCompileFromFile(
-		L"BasicPS.hlsl", // filename
-		nullptr,		// optional macros
-		nullptr,		// optional include files
-		"PS_main",		// entry point
-		"ps_5_0",		// shader model (target)
-		D3DCOMPILE_DEBUG,	// shader compile options
-		0,				// effect compile options
-		&pPS,			// double pointer to ID3DBlob		
-		&errorBlob			// pointer for Error Blob messages.
-	);
-
-	// compilation failed?
-	if (FAILED(result))
-	{
-		if (errorBlob)
-		{
-			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
-			// release "reference" to errorBlob interface object
-			errorBlob->Release();
-		}
-		if (pPS)
-			pPS->Release();
-		return result;
-	}
-
-	gDevice->CreatePixelShader(pPS->GetBufferPointer(), pPS->GetBufferSize(), nullptr, &gPixelShader);
-	// we do not need anymore this COM object, so we release it.
-	pPS->Release();
-
-	return S_OK;
-}
-
-void CreateTriangleData()
-{
-	struct TriangleVertex
-	{
-		float x, y, z;
-		float r, g, b;
-	};
-
-	// Array of Structs (AoS)
-	TriangleVertex triangleVertices[3] =
-	{
-		0.0f, 0.5f, 0.0f,	//v0 pos
-		1.0f, 0.0f, 0.0f,	//v0 color
-
-		0.5f, -0.5f, 0.0f,	//v1
-		0.0f, 1.0f, 0.0f,	//v1 color
-
-		-0.5f, -0.5f, 0.0f, //v2
-		0.0f, 0.0f, 1.0f	//v2 color
-	};
-
-	// Describe the Vertex Buffer
-	D3D11_BUFFER_DESC bufferDesc;
-	memset(&bufferDesc, 0, sizeof(bufferDesc));
-	// what type of buffer will this be?
-	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	// what type of usage (press F1, read the docs)
-	bufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	// how big in bytes each element in the buffer is.
-	bufferDesc.ByteWidth = sizeof(triangleVertices);
-
-	// this struct is created just to set a pointer to the
-	// data containing the vertices.
-	D3D11_SUBRESOURCE_DATA data;
-	data.pSysMem = triangleVertices;
-
-	// create a Vertex Buffer
-	gDevice->CreateBuffer(&bufferDesc, &data, &gVertexBuffer);
-}
-
-
-void Render()
-{
-	// clear the back buffer to a deep blue
-	float clearColor[] = { 0, 0, 0, 1 };
-
-	// use DeviceContext to talk to the API
-	gDeviceContext->ClearRenderTargetView(gBackbufferRTV, clearColor);
-
-	// specifying NULL or nullptr we are disabling that stage
-	// in the pipeline
-	gDeviceContext->VSSetShader(gVertexShader, nullptr, 0);
-	gDeviceContext->HSSetShader(nullptr, nullptr, 0);
-	gDeviceContext->DSSetShader(nullptr, nullptr, 0);
-	gDeviceContext->GSSetShader(nullptr, nullptr, 0);
-	gDeviceContext->PSSetShader(gPixelShader, nullptr, 0);
-
-	UINT32 vertexSize = sizeof(float) * 6;
-	UINT32 offset = 0;
-	// specify which vertex buffer to use next.
-	gDeviceContext->IASetVertexBuffers(0, 1, &gVertexBuffer, &vertexSize, &offset);
-
-	// specify the topology to use when drawing
-	gDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	// specify the IA Layout (how is data passed)
-	gDeviceContext->IASetInputLayout(gVertexLayout);
-
-	// issue a draw call of 3 vertices (similar to OpenGL)
-	gDeviceContext->Draw(3, 0);
-}
+Camera* pCamera = nullptr;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
@@ -227,6 +33,17 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	AnimationEditorApplication app;
 	app.Init(wndHandle);
 	app.LoadStaticMeshFilesInDirectory("C:\\Repos\\AnimationEditor\\Assets");
+	pCamera = app.GetCamera();
+	//init mouse
+	{
+		RAWINPUTDEVICE Rid[1];
+		Rid[0].usUsagePage = HID_USAGE_PAGE_GENERIC;
+		Rid[0].usUsage = HID_USAGE_GENERIC_MOUSE;
+		Rid[0].dwFlags = RIDEV_INPUTSINK;
+		Rid[0].hwndTarget = wndHandle;
+		RegisterRawInputDevices(Rid, 1, sizeof(Rid[0]));
+	}
+
 	if (wndHandle)
 	{
 		
@@ -284,12 +101,36 @@ HWND InitWindow(HINSTANCE hInstance)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static int i = 0;
 	switch (message)
 	{
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	case WM_INPUT:
+	{
+		UINT rawInputSize;
+
+		GetRawInputData((HRAWINPUT)(lParam), RID_INPUT, nullptr, &rawInputSize, sizeof(RAWINPUTHEADER));
+
+		LPBYTE inputBuffer = new BYTE[rawInputSize];
+
+		UINT poo = GetRawInputData((HRAWINPUT)(lParam), RID_INPUT, inputBuffer, &rawInputSize, sizeof(RAWINPUTHEADER));
+
+		RAWINPUT* inp = (RAWINPUT*)inputBuffer;   // valid
+
+		if (inp->header.dwType == RIM_TYPEMOUSE)
+		{
+			int xPosRelative = inp->data.mouse.lLastX;
+			int yPosRelative = inp->data.mouse.lLastY;
+			float pitch = static_cast<float>(yPosRelative) / 100.f;
+			float yaw = static_cast<float>(xPosRelative) / 100.f;
+			pCamera->IncreasePitchYaw(pitch, yaw);
+		}
+		break;
 	}
+	}
+
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
